534e0885520495579a35dc89cc8a1eaf0a4526d1
Avila2203397 <avilajohnandrei97@gmail.com>
2024-11-13 12:46:51 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
+use App\Http\Controllers\Api\BookController;
 
 Route::get('/api/books', [BookController::class, 'index']);
 Route::get('/', function () {
